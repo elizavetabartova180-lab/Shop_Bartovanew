c7a04f4cd8324480321e8f3f9de9234e0602c020
student-a502 <student-a502@STUD-RM20-A502>
2025-10-30 18:12:06 +0500

    Разработанный класс Children.Бартова

---
+++ Shop_Bartova/Classes/Children.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop_Bartova.Classes
+{
+    public class Children : Shop
+    {
+        public int Age { get; set; }
+        public Children(string name, int price, string size, int age) : base(name, price)
+        {
+            this.Age = age;
+        }
+    }
+}
